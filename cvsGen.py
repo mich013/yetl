ad9ca006e242fef7816282dd761466d5b0b19d65
Michel Fisher <mich013@hotmail.com>
2019-12-18 15:19:47 -0500

    Updting csvGen to use new yaml load style Adding more configuration options to CsvGen

--- cvsGen.py
+++ cvsGen.py
@@ -1,19 +1,49 @@
 
 import random
+import sys
+import ruamel.yaml
 
-produce = ["Apple",
-          "Grapes",
-          "Almonds",
-          "Walnuts",
-          "Pears",
-          "Oranges"]
+class CsvGen():
+    def __init__(self, header=None, count=None, row=None, tablename=None):
+        self.header=headeer
+        self.count=count
+        self.row=row
+        self.tablename=tablename
 
-count = 500
-fname = "input.csv"
-with open(fname, "w+") as f:
-    f.write("Produce,Qty,Price\n")
-    for i in range(0, count):
-        p = produce[random.randint(0, len(produce)-1)]
-        qty = random.randint(1,10)
-        price = random.uniform(1.5, 10.0)
-        f.write('"{}",{},{}\n'.format(p,qty,price))  
+    @classmethod
+    def from_yaml(cls, constructor, node):
+        for m in constructor.construct_yaml_map(node):
+            pass
+        return cls(m['Header'], m['Count'], m['Row'], m['TableName'])
+
+    def __repr__(self):
+        return 'Version(header={.header}, count={.count}, row={.row}, tablenake={.tablename})'.format(self, self, self, self)
+
+
+
+
+# produce = ["Apple",
+#           "Grapes",
+#           "Almonds",
+#           "Walnuts",
+#           "Pears",
+#           "Oranges"]
+# 
+# count = 500
+# fname = "input.csv"
+# with open(fname, "w+") as f:
+#     f.write("Produce,Qty,Price\n")
+#     for i in range(0, count):
+#         p = produce[random.randint(0, len(produce)-1)]
+#         qty = random.randint(1,10)
+#         price = random.uniform(1.5, 10.0)
+#         f.write('"{}",{},{}\n'.format(p,qty,price))
+
+if __name__=="__main__":
+   yaml = ruamel.yaml.YAML(typ='safe')
+   yaml.register_class(CsvGen)
+   #yaml.register_class(Message)
+   with open('CsvGen.yml') as fp:
+       data = yaml.load(fp)
+   print(data)
+    
--- etlClases.py
+++ etlClases.py
@@ -32,6 +32,30 @@ class Tables:
         return 'Version(major={.major}, minor={.minor}, patch={.patch})'.format(self, self, self)
 
 
+class Tables:
+    def __init__(self, filename=None, Tables=None, object=None):
+        self.filename = filename
+        self.tables = [] if tables is None else tables
+        self.object = object
+        
+
+    @classmethod
+    def from_yaml(cls, constructor, node):
+        for m in constructor.construct_yaml_map(node):
+            pass
+        if 'Name' in m:
+            filename = m['FileName']
+        elif 'columns' in m:
+            columns = m['Columns']
+        else:
+            name = None
+        object = m['object'] if 'object' in m else None
+        return cls(filename, tables, object)
+
+    def __repr__(self):
+        return 'Message(filename={}, tables={}, object={})'.format(
+            self.filename, self.tables, self.object)
+    
 class CsvTable:
     def __init__(self, filename=None, Tables=None, object=None):
         self.filename = filename
@@ -50,30 +74,12 @@ class CsvTable:
         else:
             name = None
         object = m['object'] if 'object' in m else None
-        if 'DLC' in m:
-            dlc = m['DLC']
-        else:
-            dlc = None
-        if 'signals' in m:
-            signals = m['signals']
-        elif 'Signal1' in m:
-            x = 1
-            signals = []
-            while True:
-                name = "Signal{}".format(x)
-                try:
-                    signals.append(m[name])
-                except KeyError:
-                    break
-                x += 1
-        else:
-            signals = None
-        return cls(name, dlc, object, signals)
+        return cls(filename, tables, object)
 
     def __repr__(self):
-        return 'Message(name={}, DLC={}, object={}, signals{})'.format(
-            self.name, self.dlc, self.object, '[...]' if self.signals else '[]',
-        )
+        return 'Message(filename={}, tables={}, object={})'.format(
+            self.filename, self.tables, self.object)
+    
 
 if __name__=="__main__":
     yaml = ruamel.yaml.YAML(typ='safe')
